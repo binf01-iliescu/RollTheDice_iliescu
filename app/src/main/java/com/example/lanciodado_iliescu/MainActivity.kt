5a18fffac9afa992781567c13fd0739a880ee335
iliescu <iliescurobert15@gmail.com>
2025-04-08 11:35:59 +0200

    secondo commit creazione seconda activity

--- app/src/main/java/com/example/lanciodado_iliescu/MainActivity.kt
+++ app/src/main/java/com/example/lanciodado_iliescu/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rollthedice5bi
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.lanciodado_iliescu.R
+import com.example.lanciodado_iliescu.SecondActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +24,16 @@ class MainActivity : AppCompatActivity() {
         var txtTitle = findViewById<TextView>(R.id.textViewtitle)
         var imgView = findViewById<ImageView>(R.id.imageView)
         btnRoll.setOnClickListener(View.OnClickListener {
-            var mioToast = Toast.makeText(this,"DADO LANCIATO!!", Toast.LENGTH_LONG)
+            var mioToast = Toast.makeText(this, "DADO LANCIATO!!", Toast.LENGTH_LONG)
             mioToast.show()
         })
     }
+        private fun estraiNumero(): Int {
+            return (1..6).random()
+        }
+        private fun lanciaIntent(mioRandom : Int){
+            var mioIntent = Intent ( this, SecondActivity::class.java)
+            mioIntent.putExtra("NUMERO", mioRandom)
+            startActivity(mioIntent)
+        }
 }
